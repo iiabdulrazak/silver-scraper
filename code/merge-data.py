8e327e377a1a6b68ebc1974790446e8edd7e006a
Abdulrazak Osman <zaak0101@hotmail.com>
2021-05-01 23:06:53 +0300

    Solution: month total merge

--- code/merge-data.py
+++ code/merge-data.py
@@ -5,18 +5,18 @@ import pandas as pd
 #and lets concat using looping method
 path = ('../data/joData/april/')
 filesList = glob.glob(path + '*.csv')
-with open(path + 'output.csv','w') as wf:
+with open(path + 'april.csv','w') as wf:
 	for file in filesList:
 		with open(file) as rf:
 			wf.write(rf.read().strip()+"\n")
 
-print('\nProcess Done... \nAll Data Concated to: output.csv')
+print('\nProcess Done... \nAll Data Concated to: april.csv')
 
 #removing punctuation from all rows, then converting them all to int()
 #and adding columns names
 col_names = ['date','country/region', 'new_cases', 'new_deaths', 'total_cases', 'total_deaths', 'total_recovery']
 
-genData = pd.read_csv('../data/joData/april/output.csv', names=col_names)
+genData = pd.read_csv('../data/joData/april/april.csv', names=col_names)
 genData['new_cases'] = genData['new_cases'].str.replace(r'\W', '', regex=True).astype('int')
 genData['new_deaths'] = genData['new_deaths'].replace(r'\W', '', regex=True).astype('int')
 genData['total_cases'] = genData['total_cases'].str.replace(r'\W', '', regex=True).astype('int')
